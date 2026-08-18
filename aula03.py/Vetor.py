5fbdf955c88bbd26dd7512893b696a51f73d4522
joaoholiveira20 <joao.h.oliveira@ba.estudante.senai.br>
2026-08-18 16:27:45 -0300

    Add files via upload

---
+++ aula03.py/Vetor.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls")
+
+numeros = [33,11,22,54,51,52,17]
+nomes = ["Julia", "Maria", "Luiza", "Enzo"]
+
+nomes.remove("Enzo")
+
+numeros.remove(54)
+
+print(nomes)
+print(numeros)
+
+#append - insere no vetor, sempre na ultima posição
+#insert - insere um novo valor na posição desejada
+#pop - Deleta um valor por sua posição
+#remove - deleta um valor pelo seu conteúdo
+#sort - 
+#reverse - 
