bea84831db18ca85d96ad385705f493d7ad3852a
EzyoTC <enzoalvarezs11@gmail.com>
2026-08-19 16:29:11 -0300

    coisas novas

--- aula03.py/repetição.py
+++ aula03.py/repetição.py
@@ -1,12 +1,25 @@
 
-import os
-os.system("cls")
+# import os
+# os.system("cls")
     
-# For - Estrutura de repetição com variável de controle
-# while - Estrutura de repetição com condição de parada
+# # For - Estrutura de repetição com variável de controle
+# # while - Estrutura de repetição com condição de parada
 
-for i in range(61):
-    if i % 2 == 0:
-        print(f"{i} PAR")
+# for i in range(61):
+#     if i % 2 == 0:
+#         print(f"{i} - PAR")
+#     else:
+#         print(f"{i} - IMPAR")
+
+
+carrinho = []
+
+while True:
+    produto = float(input("Digite o valor do produto: "))
+    if produto == 0:
+        break
     else:
-        print(f"{i} IMPAR")
+        carrinho.append(produto)
+        
+total = sum(carrinho)
+print(f"O total da compra é: {total:.2f}")
