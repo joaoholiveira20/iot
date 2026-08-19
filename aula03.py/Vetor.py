3991972f41abea30636022094c1d9a90cf1ad451
EzyoTC <enzoalvarezs11@gmail.com>
2026-08-19 16:06:23 -0300

    laço ai

--- aula03.py/Vetor.py
+++ aula03.py/Vetor.py
@@ -1,19 +1,20 @@
 import os
 os.system("cls")
 
-numeros = [33,11,22,54,51,52,17]
+numeros = [33,11,22,54,51,52,17,33]
 nomes = ["Julia", "Maria", "Luiza", "Enzo"]
 
-nomes.remove("Enzo")
 
-numeros.remove(54)
 
-print(nomes)
-print(numeros)
 
 #append - insere no vetor, sempre na ultima posição
 #insert - insere um novo valor na posição desejada
 #pop - Deleta um valor por sua posição
 #remove - deleta um valor pelo seu conteúdo
-#sort - 
-#reverse - 
+#sort - Ordena o Vetor
+#reverse - Inverte as posições do vetor
+#len - Informar quantos valores existem no vetor
+#count - Informar quantas vezes um valor aparece no vetor
+#sum - Informar a soma de todos os valores do vetor
+#max - Informar o maior valor do vetor
+#min - Informar o menor valor do vetor
---
+++ aula03.py/repetição.py
@@ -0,0 +1,12 @@
+
+import os
+os.system("cls")
+    
+# For - Estrutura de repetição com variável de controle
+# while - Estrutura de repetição com condição de parada
+
+for i in range(61):
+    if i % 2 == 0:
+        print(f"{i} PAR")
+    else:
+        print(f"{i} IMPAR")
